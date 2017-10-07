997644e304474eeb1dec9da2a5c41239c98cd980
Tero <contact.tero@gmail.com>
2017-10-07 13:02:58 +0200

    -MOD splitscreen -ADD trigger for splitscreen state

---
+++ Assets/Scripts/Game_Controller/MainScreen.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class MainScreen : MonoBehaviour {
+
+    private Camera m_Cam;
+
+    void awake()
+    {
+        m_Cam = GetComponent<Camera>();
+    }
+
+    void OnEnable()
+    {
+        EventManager.StartListening("splitscreen", triggerState);
+    }
+
+    void OnDisable()
+    {
+        EventManager.StopListening("splitscreen", triggerState);
+    }
+
+    void triggerState()
+    {
+        m_Cam.enabled = !SplitScreen.splitState();
+    }
+}
---
+++ Assets/Scripts/Game_Controller/UIManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+    [SerializeField]
+    private Button startGameButton;
+    [SerializeField]
+    private GameObject MainMenuRoot;
+
+	void Start () {
+        startGameButton.onClick.AddListener(startGame);
+	}
+	
+	void startGame()
+    {
+        EventManager.TriggerEvent("start");
+        MainMenuRoot.SetActive(false);
+    }
+}
