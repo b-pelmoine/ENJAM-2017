7ff2f5494cf6056ef11a2272eefc3421c8889387
Tero <contact.tero@gmail.com>
2017-10-07 12:25:16 +0200

    -MOD splitscreen & ADD Event Manager & GameManager

---
+++ Assets/Scripts/Game_Controller/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameManager : MonoBehaviour {
+	
+	void Update () {
+        if (Input.GetKeyDown("x"))
+        {
+            EventManager.TriggerEvent("splitscreen");
+        }
+        if (Input.GetKeyDown("c"))
+        {
+            SplitScreen.switchState();
+        }
+    }
+
+    void OnEnable()
+    {
+        EventManager.StartListening("start", GameStart);
+    }
+
+    void OnDisable()
+    {
+        EventManager.StopListening("start", GameStart);
+    }
+
+    void GameStart()
+    {
+        Debug.Log("Game starting !");
+    }
+}
--- Assets/Scripts/Game_Controller/SplitScreen.cs
+++ Assets/Scripts/Game_Controller/SplitScreen.cs
@@ -3,17 +3,48 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SplitScreen : MonoBehaviour {
-   
 
-    Camera m_Cam;
+    static bool splitEnabled = true;
 
+    private enum POSITION{
+        LEFT,
+        RIGHT
+    }
+
+    [SerializeField]
+    private Camera m_Cam;
+    private Color m_color;
+    [SerializeField]
+    private POSITION m_pos = POSITION.LEFT;
 
 	void Start () {
-		
+        m_Cam = (Camera) gameObject.GetComponent<Camera>();
+        if (!m_Cam) Debug.LogError("Splitscreen : Missing camera object");
+        else m_color = m_Cam.backgroundColor;
+        switch(m_pos)
+        {
+            case POSITION.LEFT: { m_Cam.rect = new Rect(0.0f,0.0f,0.5f,1.0f); } break;
+            case POSITION.RIGHT: { m_Cam.rect = new Rect(0.5f,0.0f,0.5f,1.0f); } break;
+        }
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    void OnEnable()
+    {
+        EventManager.StartListening("splitscreen", triggerState);
+    }
+
+    void OnDisable()
+    {
+        EventManager.StopListening("splitscreen", triggerState);
+    }
+
+    public static void switchState()
+    {
+        SplitScreen.splitEnabled = !splitEnabled;
+    }
+
+    void triggerState()
+    {
+        m_Cam.backgroundColor = (splitEnabled) ? Color.white : m_color;
+    }
 }
