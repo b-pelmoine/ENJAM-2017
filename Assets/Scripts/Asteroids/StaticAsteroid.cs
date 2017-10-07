a530de31954e7661c13807f854136f02b0fca938
Tero <contact.tero@gmail.com>
2017-10-07 14:56:47 +0200

    -ADD static asteroid sprites & script

---
+++ Assets/Scripts/Asteroids/StaticAsteroid.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(SpriteRenderer))]
+public class StaticAsteroid : MonoBehaviour {
+    [SerializeField]
+    List<Sprite> sprites;
+	void Start () {
+        if (sprites.Count == 0) Debug.LogError("No textures found for static asteroids");
+        SpriteRenderer renderer = GetComponent<SpriteRenderer>();
+        renderer.sprite = sprites[Random.Range(0, sprites.Count-1)];
+        gameObject.AddComponent<PolygonCollider2D>();
+    }
+}
