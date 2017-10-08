0638dec199cd24d0df790874e80b581046b8e610
Tero <contact.tero@gmail.com>
2017-10-08 13:44:56 +0200

    -ADD UI Game / mainMenu & Loading

---
+++ Assets/Scripts/UI/ButtonHover.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ButtonHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+
+    public Sprite ImageHover;
+    public Sprite ImageNormal;
+
+    private Image buttonImage;
+
+    public bool state;
+
+    public void Awake()
+    {
+        buttonImage = gameObject.GetComponent<Image>();
+        state = EventSystem.current.currentSelectedGameObject == gameObject;
+    }
+
+    public void Start()
+    {
+        if (gameObject.name == "Quit")
+        {
+            Button btn = GetComponent<Button>();
+            btn.onClick.RemoveAllListeners();
+            btn.onClick.AddListener(() => { Application.Quit(); });
+        }
+        if(gameObject.name == "Start")
+        {
+            EventSystem.current.SetSelectedGameObject(gameObject);
+        }
+    }
+
+    public void Update()
+    {
+        bool newState = EventSystem.current.currentSelectedGameObject == gameObject;
+        if (state != newState)
+        {
+            PointerEventData data = new PointerEventData(EventSystem.current);
+            if (newState)
+                OnPointerEnter(data);
+            else
+                OnPointerExit(data);
+            state = newState;
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        buttonImage.sprite = ImageHover;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        buttonImage.sprite = ImageNormal;
+    }
+}
---
+++ Assets/Scripts/UI/pickRandomSpirte.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class pickRandomSpirte : MonoBehaviour {
+
+    public Sprite[] sprites;
+
+    void OnEnable()
+    {
+        EventManager.StartListening("loading", loading);
+    }
+
+    void OnDisable()
+    {
+        EventManager.StopListening("loading", loading);
+    }
+
+    void loading()
+    {
+        Image img = gameObject.GetComponent<Image>();
+        img.sprite = sprites[Random.Range(0, sprites.Length - 1)];
+        img.preserveAspect = true;
+    }
+}
